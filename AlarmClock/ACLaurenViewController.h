ae2a43cc308a96ba07cf9d9a28f6b14374edc637
Kyle Frost <kyle@kylefrostdesign.com>
2013-11-04 11:12:35 -0500

    Update Views

---
+++ AlarmClock/ACLaurenViewController.h
@@ -0,0 +1,19 @@
+//
+//  ACLaurenViewController.h
+//  AlarmClock
+//
+//  Created by Kyle Frost on 11/4/13.
+//  Copyright (c) 2013 Kyle Frost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACLaurenViewController : UIViewController {
+    
+    // Nothing to see here, move along.
+    
+}
+
+// Nothing here either.
+
+@end
---
+++ AlarmClock/ACLaurenViewController.m
@@ -0,0 +1,51 @@
+//
+//  ACLaurenViewController.m
+//  AlarmClock
+//
+//  Created by Kyle Frost on 11/4/13.
+//  Copyright (c) 2013 Kyle Frost. All rights reserved.
+//
+
+#import "ACLaurenViewController.h"
+
+@interface ACLaurenViewController ()
+
+@end
+
+@implementation ACLaurenViewController
+
+/*
+ This file, and its header file, actually do nothing to the project. In every project I do, I always find a way to leave a note or
+     have a seret page for my girlfriend, Lauren White ( http://www.twitter.com/lauren_alexis ). She doesn't code, probably will never
+     find this (and if she does, it is because I led her to it [which I already did while writing this]), but nonetheless I am
+     putting this here, in my public source code, for her to eventually see.
+ 
+ Lauren, I could not live without you. We have loved in our ups, and we have fought to love harder in our downs. Through the
+     hardest of times we have found the best in each other. We have seen that no matter what comes our way, we will be together in
+     the end, and happier than before. I hope that this lasts forever, and I could not imagine being with anyone else. You are my
+     everything, and I would do anything for you. I love you.
+ 
+*/
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
