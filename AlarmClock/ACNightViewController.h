68a7f9e16eb2a7beaa770ab2ddcfec3bd9bb69ce
Kyle Frost <kyle@kylefrostdesign.com>
2014-03-13 13:35:45 -0400

    Continue Settings, custom Night Mode

---
+++ AlarmClock/ACNightViewController.h
@@ -0,0 +1,13 @@
+//
+//  ACNightViewController.h
+//  AlarmClock
+//
+//  Created by Kyle Frost on 3/12/14.
+//  Copyright (c) 2014 Kyle Frost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACNightViewController : UITableViewController
+
+@end
