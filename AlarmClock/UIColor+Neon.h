8d8b9ddd43917f4ce0b93d23aeaca3ab6197c843
Kyle Frost <kyle.frost.dev@gmail.com>
2014-04-10 11:49:42 -0400

    Add UIColor+Neon subclass

---
+++ AlarmClock/UIColor+Neon.h
@@ -0,0 +1,134 @@
+//
+//  UIColor+Neon.h
+//  AlarmClock
+//
+//  Created by Lauren White on 4/9/14.
+//  Copyright (c) 2014 Kyle Frost Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Neon)
+
+//Blue
++ (instancetype)customBlueTintColor;
+
++ (instancetype)customBlueDayTextColor;
++ (instancetype)customBlueDayOffColor;
++ (instancetype)customBlueDayBackgroundColor;
+
++ (instancetype)customBlueNightTextColor;
++ (instancetype)customBlueNightOffColor;
++ (instancetype)customBlueNightBackgroundColor;
+
+//Coral
++ (instancetype)customCoralTintColor;
+
++ (instancetype)customCoralDayTextColor;
++ (instancetype)customCoralDayOffColor;
++ (instancetype)customCoralDayBackgroundColor;
+
++ (instancetype)customCoralNightTextColor;
++ (instancetype)customCoralNightOffColor;
++ (instancetype)customCoralNightBackgroundColor;
+
+//ElectricBlue
++ (instancetype)customElectricBlueTintColor;
+
++ (instancetype)customElectricBlueDayTextColor;
++ (instancetype)customElectricBlueDayOffColor;
++ (instancetype)customElectricBlueDayBackgroundColor;
+
++ (instancetype)customElectricBlueNightTextColor;
++ (instancetype)customElectricBlueNightOffColor;
++ (instancetype)customElectricBlueNightBackgroundColor;
+
+//Lime
++ (instancetype)customLimeTintColor;
+
++ (instancetype)customLimeDayTextColor;
++ (instancetype)customLimeDayOffColor;
++ (instancetype)customLimeDayBackgroundColor;
+
++ (instancetype)customLimeNightTextColor;
++ (instancetype)customLimeNightOffColor;
++ (instancetype)customLimeNightBackgroundColor;
+
+//Mint
++ (instancetype)customMintTintColor;
+
++ (instancetype)customMintDayTextColor;
++ (instancetype)customMintDayOffColor;
++ (instancetype)customMintDayBackgroundColor;
+
++ (instancetype)customMintNightTextColor;
++ (instancetype)customMintNightOffColor;
++ (instancetype)customMintNightBackgroundColor;
+
+//Orange
++ (instancetype)customOrangeTintColor;
+
++ (instancetype)customOrangeDayTextColor;
++ (instancetype)customOrangeDayOffColor;
++ (instancetype)customOrangeDayBackgroundColor;
+
++ (instancetype)customOrangeNightTextColor;
++ (instancetype)customOrangeNightOffColor;
++ (instancetype)customOrangeNightBackgroundColor;
+
+//Pink
++ (instancetype)customPinkTintColor;
+
++ (instancetype)customPinkDayTextColor;
++ (instancetype)customPinkDayOffColor;
++ (instancetype)customPinkDayBackgroundColor;
+
++ (instancetype)customPinkNightTextColor;
++ (instancetype)customPinkNightOffColor;
++ (instancetype)customPinkNightBackgroundColor;
+
+//Purple
++ (instancetype)customPurpleTintColor;
+
++ (instancetype)customPurpleDayTextColor;
++ (instancetype)customPurpleDayOffColor;
++ (instancetype)customPurpleDayBackgroundColor;
+
++ (instancetype)customPurpleNightTextColor;
++ (instancetype)customPurpleNightOffColor;
++ (instancetype)customPurpleNightBackgroundColor;
+
+//Red
++ (instancetype)customRedTintColor;
+
++ (instancetype)customRedDayTextColor;
++ (instancetype)customRedDayOffColor;
++ (instancetype)customRedDayBackgroundColor;
+
++ (instancetype)customRedNightTextColor;
++ (instancetype)customRedNightOffColor;
++ (instancetype)customRedNightBackgroundColor;
+
+//Scarlet
++ (instancetype)customScarletTintColor;
+
++ (instancetype)customScarletDayTextColor;
++ (instancetype)customScarletDayOffColor;
++ (instancetype)customScarletDayBackgroundColor;
+
++ (instancetype)customScarletNightTextColor;
++ (instancetype)customScarletNightOffColor;
++ (instancetype)customScarletNightBackgroundColor;
+
+//Yellow
++ (instancetype)customYellowTintColor;
+
++ (instancetype)customYellowDayTextColor;
++ (instancetype)customYellowDayOffColor;
++ (instancetype)customYellowDayBackgroundColor;
+
++ (instancetype)customYellowNightTextColor;
++ (instancetype)customYellowNightOffColor;
++ (instancetype)customYellowNightBackgroundColor;
+
+@end
---
+++ AlarmClock/UIColor+Neon.m
@@ -0,0 +1,296 @@
+//
+//  UIColor+Neon.h
+//  AlarmClock
+//
+//  Created by Lauren White on 4/9/14.
+//  Copyright (c) 2014 Kyle Frost Design. All rights reserved.
+//
+
+#import "UIColor+Neon.h"
+
+@implementation UIColor (Neon)
+
+//Blue
++ (instancetype)customBlueTintColor{
+    return [[self class] customColorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customBlueDayTextColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
++ (instancetype)customBlueDayOffColor{
+    return [[self class] customColorWithRed:24/255.0f green:198/255.0f blue:204/255.0f alpha:1.0f];
+}
++ (instancetype)customBlueDayBackgroundColor{
+    return [[self class] customColorWithRed:75/255.0f green:255/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customBlueNightTextColor{
+    return [[self class] customColorWithRed:75/255.0f green:255/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customBlueNightOffColor{
+    return [[self class] customColorWithRed:78/255.0f green:78/255.0f blue:78/255.0f alpha:1.0f];
+}
++ (instancetype)customBlueNightBackgroundColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
+
+//Coral
++ (instancetype)customCoralTintColor{
+    return [[self class] customColorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customCoralDayTextColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
++ (instancetype)customCoralDayOffColor{
+    return [[self class] customColorWithRed:170/255.0f green:84/255.0f blue:84/255.0f alpha:1.0f];
+}
++ (instancetype)customCoralDayBackgroundColor{
+    return [[self class] customColorWithRed:255/255.0f green:115/255.0f blue:115/255.0f alpha:1.0f];
+}
++ (instancetype)customCoralNightTextColor{
+    return [[self class] customColorWithRed:255/255.0f green:115/255.0f blue:115/255.0f alpha:1.0f];
+}
++ (instancetype)customCoralNightOffColor{
+    return [[self class] customColorWithRed:78/255.0f green:78/255.0f blue:78/255.0f alpha:1.0f];
+}
++ (instancetype)customCoralNightBackgroundColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
+
+        
+//ElectricBlue
++ (instancetype)customElectricBlueTintColor{
+    return [[self class] customColorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customElectricBlueDayTextColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
++ (instancetype)customElectricBlueDayOffColor{
+    return [[self class] customColorWithRed:66/255.0f green:65/255.0f blue:170/255.0f alpha:1.0f];
+}
++ (instancetype)customElectricBlueDayBackgroundColor{
+    return [[self class] customColorWithRed:0/255.0f green:97/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customElectricBlueNightTextColor{
+    return [[self class] customColorWithRed:0/255.0f green:97/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customElectricBlueNightOffColor{
+    return [[self class] customColorWithRed:78/255.0f green:78/255.0f blue:78/255.0f alpha:1.0f];
+}
++ (instancetype)customElectricBlueNightBackgroundColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
+            
+//Lime
++ (instancetype)customLimeTintColor{
+    return [[self class] customColorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customLimeDayTextColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
++ (instancetype)customLimeDayOffColor{
+    return [[self class] customColorWithRed:40/255.0f green:190/255.0f blue:0/255.0f alpha:1.0f];
+}
++ (instancetype)customLimeDayBackgroundColor{
+    return [[self class] customColorWithRed:33/255.0f green:254/255.0f blue:18/255.0f alpha:1.0f];
+}
++ (instancetype)customLimeNightTextColor{
+    return [[self class] customColorWithRed:33/255.0f green:254/255.0f blue:18/255.0f alpha:1.0f];
+}
++ (instancetype)customLimeNightOffColor{
+    return [[self class] customColorWithRed:78/255.0f green:78/255.0f blue:78/255.0f alpha:1.0f];
+}
++ (instancetype)customLimeNightBackgroundColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
+
+//Mint
++ (instancetype)customMintTintColor{
+    return [[self class] customColorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customMintDayTextColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
++ (instancetype)customMintDayOffColor{
+    return [[self class] customColorWithRed:86/255.0f green:208/255.0f blue:129/255.0f alpha:1.0f];
+}
++ (instancetype)customMintDayBackgroundColor{
+    return [[self class] customColorWithRed:115/255.0f green:255/255.0f blue:186/255.0f alpha:1.0f];
+}
++ (instancetype)customMintNightTextColor{
+    return [[self class] customColorWithRed:115/255.0f green:255/255.0f blue:186/255.0f alpha:1.0f];
+}
++ (instancetype)customMintNightOffColor{
+    return [[self class] customColorWithRed:78/255.0f green:78/255.0f blue:78/255.0f alpha:1.0f];
+}
++ (instancetype)customMintNightBackgroundColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
+
+//Orange
++ (instancetype)customOrangeTintColor{
+    return [[self class] customColorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customOrangeDayTextColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
++ (instancetype)customOrangeDayOffColor{
+    return [[self class] customColorWithRed:217/255.0f green:109/255.0f blue:0/255.0f alpha:1.0f];
+}
++ (instancetype)customOrangeDayBackgroundColor{
+    return [[self class] customColorWithRed:255/255.0f green:144/255.0f blue:0/255.0f alpha:1.0f];
+}
++ (instancetype)customOrangeNightTextColor{
+    return [[self class] customColorWithRed:255/255.0f green:144/255.0f blue:0/255.0f alpha:1.0f];
+}
++ (instancetype)customOrangeNightOffColor{
+    return [[self class] customColorWithRed:78/255.0f green:78/255.0f blue:78/255.0f alpha:1.0f];
+}
++ (instancetype)customOrangeNightBackgroundColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
+
+//Pink
++ (instancetype)customPinkTintColor{
+    return [[self class] customColorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customPinkDayTextColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
++ (instancetype)customPinkDayOffColor{
+    return [[self class] customColorWithRed:217/255.0f green:34/255.0f blue:102/255.0f alpha:1.0f];
+}
++ (instancetype)customPinkDayBackgroundColor{
+    return [[self class] customColorWithRed:255/255.0f green:17/255.0f blue:122/255.0f alpha:1.0f];
+}
++ (instancetype)customPinkNightTextColor{
+    return [[self class] customColorWithRed:255/255.0f green:17/255.0f blue:122/255.0f alpha:1.0f];
+}
++ (instancetype)customPinkNightOffColor{
+    return [[self class] customColorWithRed:78/255.0f green:78/255.0f blue:78/255.0f alpha:1.0f];
+}
++ (instancetype)customPinkNightBackgroundColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
+
+//Purple
++ (instancetype)customPurpleTintColor{
+    return [[self class] customColorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customPurpleDayTextColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
++ (instancetype)customPurpleDayOffColor{
+    return [[self class] customColorWithRed:146/255.0f green:75/255.0f blue:199/255.0f alpha:1.0f];
+}
++ (instancetype)customPurpleDayBackgroundColor{
+    return [[self class] customColorWithRed:199/255.0f green:126/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customPurpleNightTextColor{
+    return [[self class] customColorWithRed:199/255.0f green:126/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customPurpleNightOffColor{
+    return [[self class] customColorWithRed:78/255.0f green:78/255.0f blue:78/255.0f alpha:1.0f];
+}
++ (instancetype)customPurpleNightBackgroundColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
+
+//Red
++ (instancetype)customRedTintColor{
+    return [[self class] customColorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customRedDayTextColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
++ (instancetype)customRedDayOffColor{
+    return [[self class] customColorWithRed:161/255.0f green:42/255.0f blue:42/255.0f alpha:1.0f];
+}
++ (instancetype)customRedDayBackgroundColor{
+    return [[self class] customColorWithRed:255/255.0f green:0/255.0f blue:42/255.0f alpha:1.0f];
+}
++ (instancetype)customRedNightTextColor{
+    return [[self class] customColorWithRed:255/255.0f green:0/255.0f blue:42/255.0f alpha:1.0f];
+}
++ (instancetype)customRedNightOffColor{
+    return [[self class] customColorWithRed:78/255.0f green:78/255.0f blue:78/255.0f alpha:1.0f];
+}
++ (instancetype)customRedNightBackgroundColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
+
+//Scarlet
++ (instancetype)customScarletTintColor{
+    return [[self class] customColorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customScarletDayTextColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
++ (instancetype)customScarletDayOffColor{
+    return [[self class] customColorWithRed:217/255.0f green:65/255.0f blue:0/255.0f alpha:1.0f];
+}
++ (instancetype)customScarletDayBackgroundColor{
+    return [[self class] customColorWithRed:255/255.0f green:79/255.0f blue:26/255.0f alpha:1.0f];
+}
++ (instancetype)customScarletNightTextColor{
+    return [[self class] customColorWithRed:255/255.0f green:79/255.0f blue:26/255.0f alpha:1.0f];
+}
++ (instancetype)customScarletNightOffColor{
+    return [[self class] customColorWithRed:78/255.0f green:78/255.0f blue:78/255.0f alpha:1.0f];
+}
++ (instancetype)customScarletNightBackgroundColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
+
+//Yellow
++ (instancetype)customYellowTintColor{
+    return [[self class] customColorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0f];
+}
++ (instancetype)customYellowDayTextColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
++ (instancetype)customYellowDayOffColor{
+    return [[self class] customColorWithRed:196/255.0f green:203/255.0f blue:16/255.0f alpha:1.0f];
+}
++ (instancetype)customYellowDayBackgroundColor{
+    return [[self class] customColorWithRed:234/255.0f green:255/255.0f blue:0/255.0f alpha:1.0f];
+}
++ (instancetype)customYellowNightTextColor{
+    return [[self class] customColorWithRed:234/255.0f green:255/255.0f blue:0/255.0f alpha:1.0f];
+}
++ (instancetype)customYellowNightOffColor{
+    return [[self class] customColorWithRed:78/255.0f green:78/255.0f blue:78/255.0f alpha:1.0f];
+}
++ (instancetype)customYellowNightBackgroundColor{
+    return [[self class] customColorWithRed:55/255.0f green:55/255.0f blue:55/255.0f alpha:1.0f];
+}
+
+// Custom Color Methods
++ (instancetype)customColorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha {
+    static NSCache *cache = nil;
+    
+    if(!cache) {
+        cache = [[NSCache alloc] init];
+        
+        [cache setName:@"UIColor+Neon"];
+    }
+    
+    NSString *cacheKey = [[self class] cacheKeyWithRed:red green:green blue:blue alpha:alpha];
+    
+    UIColor *color = [cache objectForKey:cacheKey];
+    
+    if(!color) {
+        color = [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+        
+        [cache setObject:color forKey:cacheKey];
+    }
+    
+    return color;
+}
+
++ (NSString *)cacheKeyWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha {
+    return [NSString stringWithFormat:@"%.2f%.2f%.2f%.2f", red, green, blue, alpha];
+}
+
+
+
+@end
